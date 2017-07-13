88c09fbd502b800488d55cffc4a00ddb181eb09b
Zhang Zhang <zhz46@pitt.edu>
2017-07-13 00:54:08 -0400

    content based rs v2, using doc2vec with centroid and wm methods

--- furniture/preprocess.py
+++ furniture/preprocess.py
@@ -8,7 +8,7 @@ from glob import glob
 def data_load(files='../../desktop/data/*.json'):
     # read all furniture jsons
     data = []
-    for file_name in glob('../../desktop/data/*.json'):
+    for file_name in glob(files):
         with open(file_name) as f:
             temp = json.load(f)
             for sku in temp:
@@ -36,16 +36,21 @@ def data_load(files='../../desktop/data/*.json'):
 def pre_process(raw_data):
     # convert list into dataframe
     raw_df = pd.DataFrame(raw_data)
-    # combine skus that don't have groups and primary sku(first one) in each group
+    # divide no_group skus, primary skus and secondary skus
     no_group_df = raw_df.loc[raw_df.group_id.isnull()]
     group_df = raw_df.loc[raw_df.group_id.notnull()]
     primary_df = group_df.groupby('group_id').apply(lambda x: x.iloc[0])
-    raw_df = pd.concat([primary_df, no_group_df]).reset_index()
+    second_df = group_df.groupby('group_id').apply(lambda x: x.iloc[1:])
+    # concat above components in a better order for later
+    raw_df = pd.concat([primary_df, no_group_df, second_df]).reset_index()
+    # save index for numpy array
+    raw_df['original_index'] = raw_df.index
 
     # select key features
     # features = ['title', 'category_id', 'category_level_0', 'category_level_1',
     #             'brand', 'attributes', 'price_hint', 'description', 'sku_id']
-    features = ['products', 'parentProducts', 'brand', 'price_hint', 'title', 'category_id', 'id']
+    features = ['products', 'parentProducts', 'brand', 'price_hint',
+                'title', 'category_id', 'group_id', 'id', 'original_index']
     fts = {}
     for i in range(len(features)):
         fts[features[i]] = i
@@ -57,5 +62,21 @@ def pre_process(raw_data):
     # fill missing
     df.price_hint.fillna(df.price_hint.median(), inplace=True)
     # drop two skus that do not have 'meaningful' titles
-    df = df.drop(df.index[[76774, 113749]])
+    # df = df.drop(df.index[[76774, 113749]])
     return (df, fts)
+
+
+def map_generate(df):
+    # map secondary sku index to its primary sku index
+    second2primary = {}
+    # map primary sku index to its group index list
+    primary_neighbor = {}
+    # map group_id to its group index list
+    group_map = df.groupby('group_id').groups
+    for values in group_map.values():
+        primary_neighbor[values[0]] = values
+        for i in range(1, len(values)):
+            if values[i] not in second2primary:
+                second2primary[values[i]] = values[0]
+    return second2primary, primary_neighbor
+
--- furniture/tf_idf.py
+++ furniture/tf_idf.py
@@ -10,7 +10,7 @@ from sklearn.decomposition import TruncatedSVD
 from sklearn.preprocessing import Normalizer
 from sklearn.pipeline import make_pipeline
 from nltk.stem.snowball import SnowballStemmer
-from preprocess import data_load, pre_process
+from preprocess import data_load, pre_process, map_generate
 from distance import mixed_dist
 
 
@@ -66,17 +66,36 @@ title_mat = title_process(titles)
 # combine title and other features
 mat = np.concatenate((df.values, title_mat), axis=1)
 
+# build two maps for query
+second2primary, primary_neighbor = map_generate(df)
+
+
 # generate top-k recommendation list given an index
-def query(ind, k=30, dist=mixed_dist, data=mat, fts=fts):
-    # get id and category_id for given index
-    idx = data[ind, fts['id']]
+def query(query_ind, k=30, dist=mixed_dist, data=mat, fts=fts,
+          second2primary=second2primary, primary_neighbor=primary_neighbor):
+
+    # set a switch for secondary sku to primary sku
+    switch = 0
+    # store the start index of secondary sku
+    second_start = len(mat) - len(second2primary)
+
+    if query_ind >= second_start:    # if query sku is a secondary sku
+        ind = second2primary[query_ind]
+        switch = 1
+        if ind >= second_start:
+            raise ValueError('Not a primary sku')
+    else:                      # if query sku is a primary sku or non-group sku
+        ind = query_ind
+
+    cal_data = data[ind]
     cate_id = data[ind, fts['category_id']]
     # get candidate data and calculate distance
-    indices = np.arange(len(data))
-    temp_data = data[(data[:, fts['category_id']] == cate_id) & (indices != ind)]
+    temp_data = data[:second_start]
+    indices = np.arange(len(temp_data))
+    temp_data = temp_data[(temp_data[:, fts['category_id']] == cate_id) & (indices != ind)]
     if len(temp_data) == 0:
-        return [{'idX': idx, 'idY': idx, 'score': 1, 'method': 'content_based_v1'}]
-    dist_mat = np.apply_along_axis(dist, axis=1, arr=temp_data, a=data[ind, :], fts=fts)
+        return [{'idX': cal_data[fts['id']], 'idY': cal_data[fts['id']], 'score': 1, 'method': 'content_based_v2.1'}]
+    dist_mat = np.apply_along_axis(dist, axis=1, arr=temp_data, a=cal_data, fts=fts)
     if len(temp_data) > k:
         temp_ind = np.argpartition(dist_mat, k)[:k]
         top_ind = temp_ind[np.argsort(dist_mat[temp_ind])]
@@ -84,15 +103,33 @@ def query(ind, k=30, dist=mixed_dist, data=mat, fts=fts):
         top_ind = np.argsort(dist_mat)
     top_score = dist_mat[top_ind]
     top_idy = temp_data[top_ind, fts['id']]
+
+    # get original index of top skus
+    top_ori_index = temp_data[top_ind, fts['original_index']]
     result = []
-    for idy, value in zip(top_idy, top_score):
+    for i in range(len(top_idy)):
+        # if recommend sku is primary sku, calculate nearest one in the group
+        if top_ori_index[i] < len(primary_neighbor):
+            group_i = primary_neighbor[top_ori_index[i]]
+            group_data = data[group_i]
+            # if original query sku is secondary, then use original data to calculate
+            if switch == 1:
+                cal_data = data[query_ind]
+            group_dist = np.apply_along_axis(dist, axis=1, arr=group_data, a=cal_data, fts=fts)
+            best_ind = np.argmin(group_dist)
+            value = group_dist[best_ind]
+            idy = group_data[best_ind, fts['id']]
+        else:
+            idy = top_idy[i]
+            value = top_score[i]
         pair = dict()
-        pair['idX'] = idx
+        pair['idX'] = data[query_ind, fts['id']]
         pair['idY'] = idy
-        pair['method'] = 'content_based_v1'
+        pair['method'] = 'content_based_v2.1'
         pair['score'] = 1 - value
         result.append(pair)
     return result
+
 
 # parallel computing
 def parallel(func, chunk, p=6):
@@ -111,11 +148,11 @@ with open('tfidf_rs.json', 'w') as f:
     json.dump(rs_output, f)
 
 
-    # output data.json
-    # output = ['id', 'brand', 'image_url', 'title', 'category_id']
-    # df_output = raw_df[output]
-    # df_output = df_output.rename(columns={'category_id':'node_id'})
-    # df_output.to_json('data.json', orient='records')
+# output data.json
+# output = ['id', 'brand', 'image_url', 'title', 'category_id']
+# df_output = raw_df[output]
+# df_output = df_output.rename(columns={'category_id':'node_id'})
+# df_output.to_json('data.json', orient='records')
 
 
 
